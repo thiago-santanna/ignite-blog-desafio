2e358e388792c82d0444a4e5def756fd44289904
xthiagosant <uzmaki_thiago@outlook.com>
2021-04-11 14:10:46 -0300

    solucao do desafio

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import styles from './header.module.scss';
+
+function Header(): JSX.Element {
+  return (
+    <header className={styles.postHeader}>
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="logo" className={styles.logo} />
+        </a>
+      </Link>
+    </header>
+  );
 }
+
+export default Header;
